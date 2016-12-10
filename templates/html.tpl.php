e4a2c27822f27877fb58a20d105636424021ca39
Anthony K. @Big Mac <off.koss@gmail.com>
2016-12-10 20:24:38 +0300

    Add favicons

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -48,6 +48,23 @@
 <!--[if IE 8]><html class="no-js lt-ie9" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>"> <!--<![endif]-->
 <head>
+  <link rel="apple-touch-icon" sizes="57x57" href="/<?php print path_to_theme(); ?>/images/favicons/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="/<?php print path_to_theme(); ?>/images/favicons/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/<?php print path_to_theme(); ?>/images/favicons/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/<?php print path_to_theme(); ?>/images/favicons/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="/<?php print path_to_theme(); ?>/images/favicons/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="/<?php print path_to_theme(); ?>/images/favicons/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="/<?php print path_to_theme(); ?>/images/favicons/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/<?php print path_to_theme(); ?>/images/favicons/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/<?php print path_to_theme(); ?>/images/favicons/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="/<?php print path_to_theme(); ?>/images/favicons/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/<?php print path_to_theme(); ?>/images/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="/<?php print path_to_theme(); ?>/images/favicons/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/<?php print path_to_theme(); ?>/images/favicons/favicon-16x16.png">
+  <link rel="manifest" href="/<?php print path_to_theme(); ?>/images/favicons/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="/<?php print path_to_theme(); ?>/images/favicons/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
